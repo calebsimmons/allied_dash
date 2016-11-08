acbc52e8eec837df015be7c8c72205682452a607
CalebDev <CalebDev>
2016-11-08 17:04:52 -0500

    added charts for loans closed in last 30 days

---
+++ dashboards/utils.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def month_delta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
+    if not m: m = 12
+    d = min(date.day, [31,
+        29 if y%4==0 and not y%400==0 else 28,31,30,31,30,31,31,30,31,30,31][m-1])
+    return date.replace(day=d,month=m, year=y)
+
+if __name__ == "__main__":
+    for m in range(-12, 12):
+        print(month_delta(date.today(), m))
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,15 +1,74 @@
+import collections
+
+from datetime import date, timedelta, time
+from django.db.models import Count
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-import arrow
+from loans.models import ClosedLoan
+from .utils import month_delta
 
-class ClosedLoansDashView(TemplateView):
+
+class ClosedLoansDashView(ListView):
+    context_object_name = 'closed_loans'
     template_name = 'dashboards/closed_loans.html'
+    fields = ('loan_number','funds_sent_date')
+    model = ClosedLoan
 
     def get_context_data(self, **kwargs):
+        kwargs['object_list'] = ClosedLoan.objects.all()
         context = super(ClosedLoansDashView, self).get_context_data(**kwargs)
         context['thirty_days_closed'] = self.thirty_days_closed()
+        context['thirty_days_closed_cml'] = self.thirty_days_closed_cml()
         return context
 
     def thirty_days_closed(self):
-        pass    
+        final_data = collections.OrderedDict()
+        day = date.today() + timedelta(-30)
+        for i in range(1,30):
+            day = day + timedelta(1)
+            count = ClosedLoan.objects.filter(
+                    est_funding_date__year=str(day.year)
+                ).filter(
+                    est_funding_date__month=str(day.month)
+                ).filter(
+                    est_funding_date__day=str(day.day)
+                ).count()
+
+
+            final_data[str(day)] = count
+
+        return {
+            'day_labels': list(final_data.keys()),
+            'counts': list(final_data.values())
+        }
+
+    def thirty_days_closed_cml(self):
+        final_data = collections.OrderedDict()
+        day = date.today() + timedelta(-30)
+        sum = 0
+        for i in range(1,30):
+            day = day + timedelta(-1)
+            count = ClosedLoan.objects.filter(
+                    est_funding_date__year=str(day.year)
+                ).filter(
+                    est_funding_date__month=str(day.month)
+                ).filter(
+                    est_funding_date__day=str(day.day)
+                ).count()
+
+            sum += count
+            final_data[str(day)] = sum
+
+        return {
+            'day_labels': list(final_data.keys()),
+            'sums': list(final_data.values())
+        }
+
+    def last_month_closed_by_type(self):
+        last_month = month_delta(date.today(), -1)
+        qs = ClosedLoan.objects.filter(
+                est_funding_date__month=str(last_month.month)
+            ).values('loan_type').annotate(total=Count('loan_type'))
+
+        return qs    
